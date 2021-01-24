f71e927d940766daffe7bf7711905682e0cbf3a6
Jacob Evans <cloud887@gmail.com>
2021-01-24 14:29:45 -0600

    TSDX didnt work on old project

--- firebase.js
+++ firebase.js
@@ -1,8 +1,8 @@
-import admin, { ServiceAccount } from 'firebase-admin';
+import admin from 'firebase-admin';
 import serviceAccount from './serviceAccountKey.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as ServiceAccount),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_URL,
 });
 
