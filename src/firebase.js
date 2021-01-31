61481a2444a7e200a6038d70736add0c58f4aee2
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2021-01-31 16:13:11 +1100

    feat: TypeScript config

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,9 @@ import admin from 'firebase-admin';
 import serviceAccount from '../serviceAccountKey.json';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(
+    JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
+  ),
   databaseURL: process.env.FIREBASE_URL,
 });
 
