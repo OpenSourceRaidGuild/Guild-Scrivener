a3a5e7d4873675cd393e65fa060f1f08effdae0c
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2021-02-08 15:41:28 +1100

    test: complete raid

--- src/raids/completeRaid.test.ts
+++ src/raids/completeRaid.test.ts
@@ -1,13 +1,148 @@
-import { buildRepositoryEvent } from '../testUtils/dataFactory';
+import {
+  buildRepository,
+  buildRepositoryEvent,
+} from '../testUtils/dataFactory';
+import { firestore } from '../testUtils/firebaseUtils';
+import { collections } from '../firebase';
 import runOctokitWebhook from '../testUtils/runOctokitWebhook';
 import completeRaid from './completeRaid';
+import { RaidStats } from './types/raidStats';
+import { rest, server } from '../testUtils/msw';
 
-it('smoke tests', async () => {
+it(`does not complete a raid if repository is not a fork`, async () => {
+  const repositoryArchivedEvent = buildRepositoryEvent({
+    eventType: 'archived',
+    isFork: false,
+  });
   const result = await runOctokitWebhook(() =>
-    completeRaid(
-      buildRepositoryEvent({
-        eventType: 'archived',
-      })
-    )
+    completeRaid(repositoryArchivedEvent)
   );
+
+  const raidDocsSnapshot = await firestore
+    .collection(collections.raidStats)
+    .get();
+  expect(raidDocsSnapshot.docs).toHaveLength(0);
+
+  const sanitizedStdOut = result.stdOut.replace(
+    new RegExp(repositoryArchivedEvent.id, 'g'),
+    'EVENT_ID'
+  );
+  expect(sanitizedStdOut).toMatchInlineSnapshot(`
+    "[96m- Processing repository archived event 'EVENT_ID'[39m
+    [91m✖ Event 'EVENT_ID' did not meet criteria for Raid creation: Repository was not a fork[39m"
+  `);
+});
+
+it(`does not complete a raid if no active raid exists for the dungeon`, async () => {
+  const repositoryArchivedEvent = buildRepositoryEvent({
+    eventType: 'archived',
+  });
+  const result = await runOctokitWebhook(() =>
+    completeRaid(repositoryArchivedEvent)
+  );
+
+  const raidDocsSnapshot = await firestore
+    .collection(collections.raidStats)
+    .get();
+  expect(raidDocsSnapshot.docs).toHaveLength(0);
+
+  const sanitizedStdOut = result.stdOut
+    .replace(new RegExp(repositoryArchivedEvent.id, 'g'), 'EVENT_ID')
+    .replace(/(\w+|\w+\.\w+)\/((\w+(-\w+)+)|\w+)/g, 'OWNER/REPO');
+  expect(sanitizedStdOut).toMatchInlineSnapshot(`
+    "[96m- Processing repository archived event 'EVENT_ID'[39m
+    [91m✖ No active Raid exists for OWNER/REPO associated with event 'EVENT_ID'[39m"
+  `);
+});
+
+it(`does not complete a raid if more than one active raid exists for the dungeon`, async () => {
+  // Setup an existing raid
+  const raidRepo = buildRepository();
+  const raidStats: RaidStats = {
+    additions: 0,
+    changedFiles: 0,
+    commits: 0,
+    contributors: {},
+    deletions: 0,
+    dungeon: String(raidRepo.parent?.full_name),
+    status: 'active',
+    title: 'Potato',
+  };
+  await firestore.collection(collections.raidStats).add(raidStats);
+  await firestore.collection(collections.raidStats).add(raidStats);
+
+  server.use(
+    rest.get('https://api.github.com/repos/:owner/:repo', (req, res, ctx) => {
+      return res(ctx.json(raidRepo));
+    })
+  );
+
+  const repositoryArchivedEvent = buildRepositoryEvent({
+    eventType: 'archived',
+  });
+  const result = await runOctokitWebhook(() =>
+    completeRaid(repositoryArchivedEvent)
+  );
+
+  const raidDocsSnapshot = await firestore
+    .collection(collections.raidStats)
+    .get();
+  expect(raidDocsSnapshot.docs).toHaveLength(2);
+  expect(raidDocsSnapshot.docs.map((d) => d.data())).toStrictEqual([
+    raidStats,
+    raidStats,
+  ]);
+
+  const sanitizedStdOut = result.stdOut
+    .replace(new RegExp(repositoryArchivedEvent.id, 'g'), 'EVENT_ID')
+    .replace(/(\w+|\w+\.\w+)\/((\w+(-\w+)+)|\w+)/g, 'OWNER/REPO');
+  expect(sanitizedStdOut).toMatchInlineSnapshot(`
+    "[96m- Processing repository archived event 'EVENT_ID'[39m
+    [91m✖ Found more than one active Raid for OWNER/REPO associated with event 'EVENT_ID':  [\\"Potato\\",\\"Potato\\"][39m"
+  `);
+});
+
+it(`completes a raid when called`, async () => {
+  // Setup an existing raid
+  const raidRepo = buildRepository();
+  const raidStats: RaidStats = {
+    additions: 0,
+    changedFiles: 0,
+    commits: 0,
+    contributors: {},
+    deletions: 0,
+    dungeon: String(raidRepo.parent?.full_name),
+    status: 'active',
+    title: 'Potato',
+  };
+  const _ = await firestore.collection(collections.raidStats).add(raidStats);
+
+  server.use(
+    rest.get('https://api.github.com/repos/:owner/:repo', (req, res, ctx) => {
+      return res(ctx.json(raidRepo));
+    })
+  );
+
+  const repositoryArchivedEvent = buildRepositoryEvent({
+    eventType: 'archived',
+  });
+  const result = await runOctokitWebhook(() =>
+    completeRaid(repositoryArchivedEvent)
+  );
+
+  const raidDocsSnapshot = await firestore
+    .collection(collections.raidStats)
+    .get();
+  expect(raidDocsSnapshot.docs).toHaveLength(1);
+  expect(raidDocsSnapshot.docs.map((d) => d.data())).toStrictEqual([
+    { ...raidStats, status: 'completed' },
+  ]);
+
+  const sanitizedStdOut = result.stdOut
+    .replace(new RegExp(repositoryArchivedEvent.id, 'g'), 'EVENT_ID')
+    .replace(/(\w+|\w+\.\w+)\/((\w+(-\w+)+)|\w+)/g, 'OWNER/REPO');
+  expect(sanitizedStdOut).toMatchInlineSnapshot(`
+    "[96m- Processing repository archived event 'EVENT_ID'[39m
+    [92m✔ Completed Raid OWNER/REPO[39m"
+  `);
 });
